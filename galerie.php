6b28723723f2dc3a131adf091b6181317c7818d7
Simon AMADE <samade@PHILO.INTRA>
2018-06-12 09:57:59 +0200

    galerie commit to master

--- galerie.php
+++ galerie.php
@@ -4,12 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact</title>
+    <title>Galerie</title>
 </head>
 <body>
-    <h1>Page contact V1</h1>
-    <form method="POST">
-        <input type="text" name="message">
-    </form>
+    <h1>Hello Galerie</h1>
+    lbolblool
 </body>
 </html>
