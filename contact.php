13522e8fb95f03069d53f3879060eada5462858f
Simon AMADE <samade@PHILO.INTRA>
2018-06-12 09:39:00 +0200

    création page contact, modif index

--- contact.php
+++ contact.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Git</title>
+    <title>Contact</title>
 </head>
 <body>
-    <h1>Page d'accueil V2</h1>
-    HErro WOrld!
+    <h1>Page contact V1</h1>
+    <form action=""></form>
 </body>
 </html>
